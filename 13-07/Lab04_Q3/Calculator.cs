8aaec751ab5f8402d288c2c29cc32fc50bc2cd8e
Dark-X-Alpha <kdyatawathura@students.nsbm.ac.lk>
2023-06-13 12:03:37 +0530

    Lab sheet

---
+++ 13-07/Lab04_Q3/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04_Q3
+{
+    internal class Calculator
+    {
+        public double add(double num1, double num2) 
+        {
+            return num1 + num2;
+        }
+
+        public double sub(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double multi(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double div(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ 13-07/Lab04_q02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04_q02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius: ");
+            double r = double.Parse(Console.ReadLine());
+
+            FindValues fvalues = new FindValues();
+            double A = fvalues.area(r);
+            Console.WriteLine("Area is " + A);
+
+            double C = fvalues.circumference(r);
+            Console.WriteLine("Circumference is " + C);
+
+            Console.ReadKey();
+        }
+    }
+}
