06fadba272c8b61653875ddbbdd002e286979891
Dark-X-Alpha <kdyatawathura@students.nsbm.ac.lk>
2023-07-04 10:56:00 +0530

    Create Prac3.cs

---
+++ 04-07/Prac3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_03
+{
+    internal class Prac3
+    {
+        public int[] CreateArray(int size) 
+        {
+            int[] array = new int[size];
+
+            for (int i = 0; i < array.Length; i+=2) 
+            {
+                Console.WriteLine("Enter a value");
+                int value = Convert.ToInt32(Console.ReadLine());
+
+                array[i] = value;
+                array[i+1] = 0;
+            }
+            return array;
+        }
+    }
+}
