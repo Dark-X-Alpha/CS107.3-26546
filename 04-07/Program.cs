10f4cacefd003ad6a74ecbf975c6f3292ed50307
Dark-X-Alpha <kdyatawathura@students.nsbm.ac.lk>
2023-07-04 10:56:06 +0530

    Create Program.cs

---
+++ 04-07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of array :");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            Prac3 prac3 = new Prac3();
+            int[] array = prac3.CreateArray(size * 2);
+
+            Console.WriteLine("Enter the values for array");
+
+            for (int i = 0; i < array.Length; i++) 
+            {
+                Console.Write(array[i]+"");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
