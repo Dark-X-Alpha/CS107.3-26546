da9e455393f40178cd14903ae2f9a110cd0357b1
Dark-X-Alpha <kdyatawathura@students.nsbm.ac.lk>
2023-07-11 11:08:16 +0530

    Create Employee.cs

---
+++ 11-07/Prac_04Q1/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac_04Q1
+{
+    public class Employee
+    {
+        private int empID;
+        private string empName;
+        private string empDesignation;
+
+        public void setEmpId(int input)
+        {
+            empID = input;
+        }
+
+        public void setEmpName(string input)
+        {
+            empName = input;
+        }
+        public void setEmpDesignation(string input)
+        {
+            empDesignation = input;
+        }
+
+        public int getEmpID() 
+        {
+            return empID;
+        }
+
+        public string getEmpName() 
+        {
+            return empName;
+        }
+
+        public string getEmpDesignation()
+        {
+            return empDesignation;
+        }
+
+    }
+}
