8a7564f1fc434bcef3bb0308dc5c555d268a71f4
Dark-X-Alpha <kdyatawathura@students.nsbm.ac.lk>
2023-07-11 11:10:01 +0530

    Create Calculation.cs

---
+++ 11-07/Prac_04Q2/Calculation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac_04Q2
+{
+    public class Calculation
+    {
+        private double number1;
+        private double number2;
+
+        private void setValues(double num1, double num2) 
+        { 
+            number1 = num1;
+            number2 = num2;
+        }
+
+        public double getSum() 
+        { 
+            return number1 + number2;
+        }
+
+        public double getSub()
+        {
+            return number1 - number2;
+        }
+        public double getDiv()
+        {
+            return number1 / number2;
+        }
+
+        public double getMultip()
+        {
+            return number1 * number2;
+        }
+    }
+}
